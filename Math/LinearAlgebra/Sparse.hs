d8d39392eb6bb1002fc0ff6418bfc74306599e53
laughedelic <laughedelic@gmail.com>
2012-03-28 02:55:18 +0400

    root module that reexports everything

---
+++ Math/LinearAlgebra/Sparse.hs
@@ -0,0 +1,11 @@
+module Math.LinearAlgebra.Sparse
+( module Math.LinearAlgebra.Sparse.Matrix 
+, module Math.LinearAlgebra.Sparse.Vector
+, module Math.LinearAlgebra.Sparse.Algorithms
+)
+where
+
+import Math.LinearAlgebra.Sparse.Matrix 
+import Math.LinearAlgebra.Sparse.Vector
+import Math.LinearAlgebra.Sparse.Algorithms
+
