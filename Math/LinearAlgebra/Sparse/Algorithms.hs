faf404d7533b2a0aa7627c42c2c7c80c14ab08ee
laughedelic <laughedelic@gmail.com>
2012-03-28 02:55:58 +0400

    sub-root module that re-exports all algorithms

---
+++ Math/LinearAlgebra/Sparse/Algorithms.hs
@@ -0,0 +1,10 @@
+module Math.LinearAlgebra.Sparse.Algorithms
+( module Math.LinearAlgebra.Sparse.Algorithms.Staircase 
+, module Math.LinearAlgebra.Sparse.Algorithms.Diagonal
+, module Math.LinearAlgebra.Sparse.Algorithms.SolveLinear
+)
+where
+
+import Math.LinearAlgebra.Sparse.Algorithms.Staircase 
+import Math.LinearAlgebra.Sparse.Algorithms.Diagonal
+import Math.LinearAlgebra.Sparse.Algorithms.SolveLinear
